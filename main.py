3d8e797f011d68f8dbe6b21c3e8fd1423e111b4f
Vishal Singh <vishalsingh194071@gmail.com>
2026-01-02 22:38:07 +0530

    cleanup

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 # main.py
 
-import os
 from auth import YouTubeAuth
 from uploader import VideoUploader
 from csvJson import load_videos_from_json
-from config import VIDEO_DIRECTORY, CSV_FILE
-import json
-
+from config import JSON_FILE
 
 def main():
     youtube = YouTubeAuth.get_service()
     uploader = VideoUploader(youtube)
-    filepath="exampleJsonFile.json"
-    videosMetadata = load_videos_from_json(file_path=filepath)
+    videosMetadata = load_videos_from_json(file_path=JSON_FILE)
     for videoMetadata in videosMetadata:
         print(videoMetadata)
-        print("\n")
         print("\n")
         print("-------------------")
         uploader.uploadVideoAndThumbnail(videoMetadata)

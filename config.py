19bc549a3949419e6cee77b1c778e250f9af577f
Vishal Singh <vishalsingh194071@gmail.com>
2026-01-03 11:13:19 +0530

    nit cleanup

--- config.py
+++ config.py
@@ -18,12 +18,6 @@ YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
 
-# Paths
-VIDEO_DIRECTORY = "videos"
-CSV_FILE = "video_metadata.csv"
-JSON_FILE="ActualJsonFile.json"
-
-
 # Maximum number of times to retry before giving up.
 MAX_RETRIES = 5
 RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
